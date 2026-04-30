a4dd5e0513e6d502d022b45fe42762d33ba3d408
Kaustav Sinha <sinhakaustav22@gmail.com>
2026-05-01 03:25:42 +0530

    Update API URL to Railway

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+ baseURL: 'https://agrovolt-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
